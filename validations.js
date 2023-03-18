d980c72ed3dd5b774b9ef54a6d8348a64b68045e
mik3waz0vsk1 <romanoffsky@iqcomposite.com>
2023-03-18 16:14:30 +0200

    back до домашки

---
+++ validations.js
@@ -0,0 +1,20 @@
+import { body } from 'express-validator';
+
+export const loginValidation = [
+  body('email', 'Неверный формат почты').isEmail(),
+  body('password', 'Пароль повинен мати мінімум 5 символів').isLength({ min: 5 }),
+];
+
+export const registerValidation = [
+  body('email', 'Неверный формат почты').isEmail(),
+  body('password', 'Пароль повинен мати мінімум 5 символів').isLength({ min: 5 }),
+  body('fullName', "Вкажіть ім'я").isLength({ min: 3 }),
+  body('avatarUrl', 'Неверная ссылка на аватарку').optional().isURL(),
+];
+
+export const postCreateValidation = [
+  body('title', 'Введите заголовок статьи').isLength({ min: 3 }).isString(),
+  body('text', 'Введите текст статьи').isLength({ min: 3 }).isString(),
+  body('tags', 'Неверный формат тэгов!').optional().isString(),
+  body('imageUrl', 'Неверная ссылка на изображение').optional().isString(),
+];
